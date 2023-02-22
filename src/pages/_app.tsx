79a24503a58e144bf1568247d9f3276f4abac159
Van Brantley <van.brantley@gmail.com>
2023-02-22 10:44:59 -0500

    add garment to database

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -35,7 +35,7 @@ export default function MyApp(props: MyAppProps) {
         <ThemeProvider theme={theme}>
           {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
           <CssBaseline />
-          <Header />
+          {/* <Header /> */}
           <Component {...pageProps} />
         </ThemeProvider>
       </AuthContext>
