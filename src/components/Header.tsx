d966b892f913b003b5bc25b42e9fd21208795d9e
Van Brantley <van.brantley@gmail.com>
2023-06-22 19:06:22 -0400

    Changing modes in header hides menu + dynamic imports for Layout components

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,12 +16,12 @@ interface IHeaderProps {
 const Header = observer(function Header(props: IHeaderProps) {
 
     const store = useContext(AppStoreContext);
-    const { closetMode, handleModeChange } = store;
+    const { closetMode, handleModeChange, navbarOpen, setNavbarOpen } = store;
 
     const { user } = useUser();
     const router = useRouter();
 
-    const [navbarOpen, setNavbarOpen] = useState<boolean>(false);
+    // const [navbarOpen, setNavbarOpen] = useState<boolean>(false);
 
     const signUserOut = async () => {
         // Clear the access and refresh tokens from local storage
@@ -52,7 +52,7 @@ const Header = observer(function Header(props: IHeaderProps) {
                     <span className="font-semibold text-xl tracking-tight">Avatar</span>
                 </div>
                 <div className="block lg:hidden">
-                    <button onClick={() => setNavbarOpen((prev) => !prev)} className="flex items-center px-3 py-2 border rounded text-white border-white hover:text-white hover:border-white">
+                    <button onClick={() => setNavbarOpen(!navbarOpen)} className="flex items-center px-3 py-2 border rounded text-white border-white hover:text-white hover:border-white">
                         <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
                     </button>
                 </div>
