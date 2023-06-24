a5e29583481c2bef7d7c61c5248bc399a75238bf
Van Brantley <van.brantley@gmail.com>
2023-06-23 21:24:48 -0400

    Lock handling - heart filled and area color assignment

--- src/components/PaletteList.tsx
+++ src/components/PaletteList.tsx
@@ -1,26 +1,19 @@
 import { observer } from "mobx-react-lite";
 import { useContext } from 'react';
 import { AppStoreContext } from '../context/AppStoreContext';
+import PaletteMini from './PaletteMini';
 
 const PaletteList = observer(() => {
 
     const store = useContext(AppStoreContext);
-    const { palettes, assignAreaColorsFromPalette } = store;
+    const { palettes } = store;
 
     return (
         <div className="container mx-auto">
             <div className="flex max-w-xs overflow-x-auto">
                 {palettes.map((palette, i) => {
                     const { hatColor, topColor, bottomColor, shoeColor, id } = palette;
-
-                    return (
-                        <div key={i} className="flex flex-col items-center cursor-pointer" onClick={() => assignAreaColorsFromPalette(hatColor, topColor, bottomColor, shoeColor, id)}>
-                            <div className="h-8 w-8 rounded-none" style={{ backgroundColor: hatColor }}></div>
-                            <div className="h-8 w-8 rounded-none" style={{ backgroundColor: topColor }}></div>
-                            <div className="h-8 w-8 rounded-none" style={{ backgroundColor: bottomColor }}></div>
-                            <div className="h-8 w-8 rounded-none" style={{ backgroundColor: shoeColor }}></div>
-                        </div>
-                    );
+                    return <PaletteMini key={i} hatColor={hatColor} topColor={topColor} bottomColor={bottomColor} shoeColor={shoeColor} paletteId={id} />;
                 })}
             </div>
         </div>
