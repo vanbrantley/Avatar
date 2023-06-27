7811a26b168ff9a7109b7676281d2e6e5031aead
Van Brantley <van.brantley@gmail.com>
2023-06-27 10:15:57 -0400

    Add headers to desktop and tablet layouts

--- src/components/TabletLayout.tsx
+++ src/components/TabletLayout.tsx
@@ -4,26 +4,20 @@ import ClosetMenu from './ClosetMenu';
 import { observer } from 'mobx-react-lite';
 import { useContext } from 'react';
 import { AppStoreContext } from '../context/AppStoreContext';
+import Header from './Header';
 
 
 const TabletLayout = observer(() => {
 
     const store = useContext(AppStoreContext);
-    const { closetMode } = store;
+    const { closetMode, setClosetMode, handleModeChange } = store;
 
     return (
         <>
 
-            {/* <Avatar />
-            <br></br>
-            <div className="grid grid-cols-12">
-                <div className="col-span-6">Palettes</div>
-                <div className="col-span-5">
-                    {!closetMode && <LabMenu />}
-                    {closetMode && <ClosetMenu />}
-                </div>
-
-            </div> */}
+            <Header closetMode={closetMode}
+                setClosetMode={setClosetMode}
+                handleModeChange={handleModeChange} />
 
             <br></br>
             <br></br>
