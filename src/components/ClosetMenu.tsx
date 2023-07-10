e05077f6ee721471bd8a4f7ec4d665907d3b951d
Van Brantley <van.brantley@gmail.com>
2023-07-10 14:56:22 -0400

    Direction messages when things are empty + Upload image button

--- src/components/ClosetMenu.tsx
+++ src/components/ClosetMenu.tsx
@@ -21,44 +21,46 @@ const ClosetMenu = observer(() => {
         showPicker, setShowPicker } = store;
 
     return (
-        <div className="justify-center">
+        <div className="w-full h-full flex flex-col">
+            <div className="w-full h-2/5 flex flex-col justify-center">
 
-            <SwatchesMenu />
+                <SwatchesMenu />
 
-            <div className="flex">
-                <IconButton size="large" onClick={() => setShowPicker(!showPicker)}>
-                    {showPicker ? <RemoveIcon fontSize="large" style={{ color: "white" }} /> : <AddIcon fontSize="large" style={{ color: "white" }} />}
-                </IconButton>
-                <IconButton size="large" onClick={() => randomizeOutfit()}>
-                    <ShuffleIcon fontSize="large" style={{ color: "white" }} />
-                </IconButton>
-            </div>
+                <div className="flex">
+                    <IconButton size="large" onClick={() => setShowPicker(!showPicker)}>
+                        {showPicker ? <RemoveIcon fontSize="large" style={{ color: "white" }} /> : <AddIcon fontSize="large" style={{ color: "white" }} />}
+                    </IconButton>
+                    <IconButton size="large" onClick={() => randomizeOutfit()}>
+                        <ShuffleIcon fontSize="large" style={{ color: "white" }} />
+                    </IconButton>
+                </div>
+            </div >
 
-            {showPicker && (
-                <>
+            < div className="w-full h-3/5 flex items-center" >
 
-                    <div>
-                        <div className="flex">
-                            <div className="sketch-wrapper">
-                                <SketchPicker
-                                    disableAlpha
-                                    color={selectedColor}
-                                    onChangeComplete={color => handleColorChangePicker(color.hex)}
-                                />
-                            </div>
+                {showPicker && (
+
+                    <div className="flex">
+                        <div className="sketch-wrapper">
+                            <SketchPicker
+                                disableAlpha
+                                color={selectedColor}
+                                onChangeComplete={color => handleColorChangePicker(color.hex)}
+                            />
+                        </div>
+                        <div>
+                            <Palette lock={true} handler={addColorSwatch} />
                             <div>
-                                <Palette lock={true} handler={addColorSwatch} />
-                                <div>
-                                    <IconButton size="large" onClick={() => randomizePalette()}>
-                                        <ShuffleIcon fontSize="large" style={{ color: "white" }} />
-                                    </IconButton>
-                                </div>
+                                <IconButton size="large" onClick={() => randomizePalette()}>
+                                    <ShuffleIcon fontSize="large" style={{ color: "white" }} />
+                                </IconButton>
                             </div>
                         </div>
                     </div>
-                </>
-            )}
-        </div>
+                )}
+            </div >
+
+        </div >
     );
 
 
--- src/components/MockupMenu.tsx
+++ src/components/MockupMenu.tsx
@@ -9,32 +9,53 @@ const MockupMenu = observer(() => {
     const store = useContext(AppStoreContext);
     const { shirts } = store;
 
-    // const m_paths = [
-    //     "globo_1.png", "globo_2.png", "globo_1.png", "globo_2.png",
-    //     "globo_1.png", "globo_2.png", "globo_1.png", "globo_2.png",
-    //     "globo_1.png", "globo_2.png", "globo_1.png", "globo_2.png",
-    //     "globo_1.png", "globo_2.png", "globo_1.png", "globo_2.png",
-    //     "globo_1.png", "globo_2.png", "globo_1.png", "globo_2.png",
-    //     "globo_1.png", "globo_2.png", "globo_1.png", "globo_2.png",
-    //     "globo_1.png", "globo_2.png", "globo_1.png", "globo_2.png",
-    //     "globo_1.png", "globo_2.png", "globo_1.png", "globo_2.png",
+    // const m_paths = ["testuser1/21a80e9e-86a4-45a0-9afc-bdb62e8f1e98", "testuser1/6f3207e8-73e7-4e8e-8f1d-341fd3b01ff3",
+    //     "testuser1/21a80e9e-86a4-45a0-9afc-bdb62e8f1e98", "testuser1/6f3207e8-73e7-4e8e-8f1d-341fd3b01ff3",
+    //     "testuser1/21a80e9e-86a4-45a0-9afc-bdb62e8f1e98", "testuser1/6f3207e8-73e7-4e8e-8f1d-341fd3b01ff3",
+    //     "testuser1/21a80e9e-86a4-45a0-9afc-bdb62e8f1e98", "testuser1/6f3207e8-73e7-4e8e-8f1d-341fd3b01ff3",
+    //     "testuser1/21a80e9e-86a4-45a0-9afc-bdb62e8f1e98", "testuser1/6f3207e8-73e7-4e8e-8f1d-341fd3b01ff3",
+    //     "testuser1/21a80e9e-86a4-45a0-9afc-bdb62e8f1e98", "testuser1/6f3207e8-73e7-4e8e-8f1d-341fd3b01ff3",
+    //     "testuser1/21a80e9e-86a4-45a0-9afc-bdb62e8f1e98", "testuser1/6f3207e8-73e7-4e8e-8f1d-341fd3b01ff3",
+    //     "testuser1/21a80e9e-86a4-45a0-9afc-bdb62e8f1e98", "testuser1/6f3207e8-73e7-4e8e-8f1d-341fd3b01ff3",
+    //     "testuser1/21a80e9e-86a4-45a0-9afc-bdb62e8f1e98", "testuser1/6f3207e8-73e7-4e8e-8f1d-341fd3b01ff3",
+    //     "testuser1/21a80e9e-86a4-45a0-9afc-bdb62e8f1e98", "testuser1/6f3207e8-73e7-4e8e-8f1d-341fd3b01ff3",
+    //     "testuser1/21a80e9e-86a4-45a0-9afc-bdb62e8f1e98", "testuser1/6f3207e8-73e7-4e8e-8f1d-341fd3b01ff3",
+    //     "testuser1/21a80e9e-86a4-45a0-9afc-bdb62e8f1e98", "testuser1/6f3207e8-73e7-4e8e-8f1d-341fd3b01ff3",
+    //     "testuser1/21a80e9e-86a4-45a0-9afc-bdb62e8f1e98", "testuser1/6f3207e8-73e7-4e8e-8f1d-341fd3b01ff3"
     // ];
 
     return (
 
         <>
 
-            <ImageDropzone />
+            <div className="flex flex-col h-full">
 
-            <br></br>
+                <div className="h-5/6 overflow-auto overscroll-y-contain">
 
-            <div className="grid grid-cols-3 gap-4">
+                    {(shirts.length === 0) ? (
+                        <div>
+                            {/* <p>Click button below to upload an image of a shirt to be displayed on the Avatar</p> */}
+                            <p>Upload an image of a shirt to display on the Avatar using the button below</p>
 
-                {shirts.map((path, index) => (
-                    <div key={index} className="flex justify-center">
-                        <MockupResult path={path} />
-                    </div>
-                ))}
+                        </div>
+                    ) : (
+                        <div className="grid grid-cols-3 gap-4">
+
+                            {shirts.map((path, index) => (
+                                <div key={index} className="flex justify-center">
+                                    <MockupResult path={path} />
+                                </div>
+                            ))}
+                        </div>
+
+                    )}
+
+
+                </div>
+
+                <div className="h-1/6">
+                    <ImageDropzone />
+                </div>
 
             </div>
 
